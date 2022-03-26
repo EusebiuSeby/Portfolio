05fe25a1a5b503f0f12ab1cd3f97dd21748ac1cb
EusebiuSeby <51457561+EusebiuSeby@users.noreply.github.com>
2022-03-26 13:51:50 +0200

    Add files via upload

---
+++ CovidCases/CovidCases/Model/CountryData.swift
@@ -0,0 +1,13 @@
+//
+//  CountryModel.swift
+//  CovidCases
+//
+//  Created by Pudilic Seby on 10/02/2022.
+//  Copyright © 2022 Pudililc Seby. All rights reserved.
+//
+
+import Foundation
+
+struct CountryData: Decodable {
+    let Confirmed: Int
+}
